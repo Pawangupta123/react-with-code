d9d6620a9f4525a8199c67b2966412daf3243b31
pawan kumar <113826393+Pawangupta123@users.noreply.github.com>
2025-06-04 08:06:57 +0000

    Context API Pro dicuusion

---
+++ 08miniContext/src/Context/UserContext.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const UserContext = React.createContext()
+
+
+export default UserContext;
+// This code creates a UserContext using React's Context API.
+// It allows components to share user-related data without passing props down manually.
